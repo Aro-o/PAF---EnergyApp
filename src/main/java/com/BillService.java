4b4154d2c9ed7654919197ebadf228d544bc04b2
akeel <it20068264@my.sliit.lk>
2022-04-13 13:25:12 +0530

    bill manangement delete function corrected

--- src/main/java/com/BillService.java
+++ src/main/java/com/BillService.java
@@ -39,6 +39,24 @@ public class BillService {
 	}
 
 
+	@DELETE
+	@Path("/delete")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_XML)
+	public String deleteBill(String itemData) {
+		//Convert the input string to an XML document
+		 Document doc = Jsoup.parse(itemData, "", Parser.xmlParser()); 
+		 
+		//Read the value from the element <itemID>
+		 String acno = doc.select("acno").text();
+		 String year = doc.select("year").text();
+		 String month = doc.select("month").text();
+		 
+		 String output = billObject.deleteBill(acno, year, month);
+		 return output;
+	}
+
+
 	
 	
 

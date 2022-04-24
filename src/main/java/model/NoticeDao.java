f8e67b56858f54e1115c914a6b97e2888a08dbe5
Aro-o <aisharoo80@gmail.com>
2022-04-24 12:24:45 +0530

    notices updated

--- src/main/java/model/NoticeDao.java
+++ src/main/java/model/NoticeDao.java
@@ -9,39 +9,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NoticeDao {
+	private String jdbcURL = "jdbc:mysql://localhost:3306/powergridsystem?useSSL=false";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "";
 
-	private String jdbcURL = "jdbc:mysql://localhostt:3306/powergridsystem?useSSL=false";
-	private String jdbcUsername ="root";
-	private String jdbcPassword = "rootpasswordgiven";
-	private String jdbcDriver = "com.mysql.jdbc.Driver";
-	
-	private static final String INSERT_NOTICES_SQL="INSERT INTO notices" + " (topic, areasAffected, date, details) VALUES"
-			+ "(?,?,?,?);";
-	private static final String SELECT_NOTICE_BY_ID = "select id,topic,areasAffected,date,details from notices where id=?";
+	private static final String INSERT_NOTICES_SQL = "INSERT INTO notices" + "  (topic, areasAffected, date, details) VALUES "
+			+ " (?, ?, ?, ?);";
+
+	private static final String SELECT_NOTICE_BY_ID = "select id,topic,areasAffected,date,details from users where id =?";
 	private static final String SELECT_ALL_NOTICES = "select * from notices";
-	private static String DELETE_NOTICES_SQL = "delete from notices where id = ?;";
-	private static final String UPDATE_NOTICES_SQL = "update notices set topic =?,areasAffected=?,date=?,details=? where id=?;";
-	
+	private static final String DELETE_NOTICES_SQL = "delete from notices where id = ?;";
+	private static final String UPDATE_NOTICES_SQL = "update notices set topic = ?,areasAffected= ?, date =?, details=? where id = ?;";
+
 	public NoticeDao() {
 	}
-	
+
 	protected Connection getConnection() {
 		Connection connection = null;
 		try {
-			Class.forName(jdbcDriver);
+			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-		}catch(SQLException e) {
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return connection;
 	}
-	
-	//insert notice
-	public void insertNotice(Notice notice) throws SQLException{
+
+	public void insertNotice(Notice notice) throws SQLException {
 		System.out.println(INSERT_NOTICES_SQL);
-		try(Connection connection = getConnection();
+		// try-with-resource statement will auto close the connection.
+		try (Connection connection = getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_NOTICES_SQL)) {
 			preparedStatement.setString(1, notice.getTopic());
 			preparedStatement.setString(2, notice.getAreasAffected());
@@ -49,50 +50,49 @@ public class NoticeDao {
 			preparedStatement.setString(4, notice.getDetails());
 			System.out.println(preparedStatement);
 			preparedStatement.executeUpdate();
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			printSQLException(e);
 		}
 	}
-	
-	//select notice by id
+
 	public Notice selectNotice(int id) {
 		Notice notice = null;
-		//step 1: Establishing a connection
-		try(Connection connection = getConnection();
-				//Step 2: Create a statement using connection object
-				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_NOTICE_BY_ID);){
+		// Step 1: Establishing a Connection
+		try (Connection connection = getConnection();
+				// Step 2:Create a statement using connection object
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_NOTICE_BY_ID);) {
 			preparedStatement.setInt(1, id);
 			System.out.println(preparedStatement);
-			//Step 3: Execute the query or update the query
+			// Step 3: Execute the query or update query
 			ResultSet rs = preparedStatement.executeQuery();
-			
-			//Step 4: Process the ResultSet Object
-			while(rs.next()) {
+
+			// Step 4: Process the ResultSet object.
+			while (rs.next()) {
 				String topic = rs.getString("topic");
 				String areasAffected = rs.getString("areasAffected");
 				String date = rs.getString("date");
 				String details = rs.getString("details");
 				notice = new Notice(id, topic, areasAffected, date, details);
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			printSQLException(e);
 		}
 		return notice;
 	}
-	
-	//select all notices
-	public List<Notice> selectAllNotices(){
+
+	public List<Notice> selectAllNotices() {
+
 		List<Notice> notices = new ArrayList<>();
-		//Step 1: Establishing a connection
+		// Step 1: Establishing a Connection
 		try (Connection connection = getConnection();
-				
-				//Step 2: Create a statement using connection object
-				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_NOTICES);){
+
+				// Step 2:Create a statement using connection object
+			PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_NOTICES);) {
 			System.out.println(preparedStatement);
-			//Step 3: Execute the query or update the query
+			// Step 3: Execute the query or update query
 			ResultSet rs = preparedStatement.executeQuery();
-			
-			//Step 4: Process the ResultSet object
+
+			// Step 4: Process the ResultSet object.
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String topic = rs.getString("topic");
@@ -101,54 +101,52 @@ public class NoticeDao {
 				String details = rs.getString("details");
 				notices.add(new Notice(id, topic, areasAffected, date, details));
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			printSQLException(e);
 		}
 		return notices;
 	}
-	
-	//update notice
-	public boolean updateNotice(Notice notice) throws SQLException{
-		boolean rowUpdated;
-		try (Connection connection = getConnection();
-				PreparedStatement statement = connection.prepareStatement(UPDATE_NOTICES_SQL);){
-					System.out.println("updated Notice:"+statement);
-					statement.setString(1, notice.getTopic());
-					statement.setString(2, notice.getAreasAffected());
-					statement.setString(3, notice.getDate());
-					statement.setString(4, notice.getDetails());
-					statement.setInt(5, notice.getId());
-					
-					rowUpdated = statement.executeUpdate() > 0;
-				}
-		return rowUpdated;
-	}
-	
-	//delete notice
+
 	public boolean deleteNotice(int id) throws SQLException {
 		boolean rowDeleted;
 		try (Connection connection = getConnection();
-				PreparedStatement statement = connection.prepareStatement(DELETE_NOTICES_SQL);){
+				PreparedStatement statement = connection.prepareStatement(DELETE_NOTICES_SQL);) {
 			statement.setInt(1, id);
 			rowDeleted = statement.executeUpdate() > 0;
 		}
 		return rowDeleted;
 	}
-	
+
+	public boolean updateNotice(Notice notice) throws SQLException {
+		boolean rowUpdated;
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(UPDATE_NOTICES_SQL);) {
+			System.out.println("updated Notice:"+statement);
+			statement.setString(1, notice.getTopic());
+			statement.setString(2, notice.getAreasAffected());
+			statement.setString(3, notice.getDate());
+			statement.setString(4, notice.getDetails());
+			statement.setInt(5, notice.getId());
+
+			rowUpdated = statement.executeUpdate() > 0;
+		}
+		return rowUpdated;
+	}
+
 	private void printSQLException(SQLException ex) {
-		for(Throwable e : ex) {
-			if(e instanceof SQLException) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
 				e.printStackTrace(System.err);
-				System.err.println("SQLState: " + ((SQLException)e).getSQLState());
-				System.err.println("Error Code: " + ((SQLException)e).getErrorCode());
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
 				System.err.println("Message: " + e.getMessage());
 				Throwable t = ex.getCause();
 				while (t != null) {
-					System.out.println("Cause " + t);
+					System.out.println("Cause: " + t);
 					t = t.getCause();
 				}
 			}
 		}
 	}
-	
+
 }

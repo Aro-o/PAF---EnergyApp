7bca83afc0be5c1639b3e3e141f90a3ac04bae8d
Aroosha Rasheed <76822194+Aro-o@users.noreply.github.com>
2022-04-25 14:34:33 +0530

    LoginServlet.java Added

---
+++ src/main/java/com/LoginServlet.java
@@ -0,0 +1,53 @@
+package com;
+
+import java.io.IOException;
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.JOptionPane;
+
+import model.*;
+
+
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private LoginDao loginDao;
+
+    public void init() {
+        loginDao = new LoginDao();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        Login login = new Login();
+        login.setUsername(username);
+        login.setPassword(password);
+
+        try {
+            if (loginDao.validate(login)) {
+                //HttpSession session = request.getSession();
+                // session.setAttribute("username",username);
+                response.sendRedirect("/PAF/");
+            } else {
+                HttpSession session = request.getSession();
+                //session.setAttribute("user", username);
+                //response.sendRedirect("login.jsp");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
